826929f7a91a4ce4ab3a6b16e74845afb738cab8
1104792744@qq.com <31415926zqh>
2021-06-24 22:04:31 +0800

    add a java file

--- src/HiWorld.java
+++ src/HiWorld.java
@@ -1,6 +1,10 @@
 public class HiWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-          System.out.println("Hello World!");
+        System.out.println("Hello World!");
+    }
+
+    private static int add(int a, int b) {
+        return a + b;
     }
 }
